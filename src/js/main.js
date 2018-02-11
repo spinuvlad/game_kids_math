38b59097fde257cec7a3b22111087de73a5bfcce
Spinu Vlad <spinu.vlad@gmail.com>
2018-02-11 21:45:48 +0200

    gfdgt

--- src/js/main.js
+++ src/js/main.js
@@ -16,11 +16,20 @@ var timerExer = document.getElementById('timer-exer');
 
 var show = document.getElementById('show');
 
-var objLevels = {lev1: pushLevel1, lev2: pushLevel2}
-var timerE, numE;
+var objLevels = {lev1: pushLevel1, lev2: pushLevel2, lev3: pushLevel3, 
+								 lev4: pushLevel4, lev5: pushLevel5, lev6: pushLevel6,
+								 lev7: pushLevel7, lev8: pushLevel8};
+
 levels[0].addEventListener('click', objLevels.lev1);
 levels[1].addEventListener('click', objLevels.lev2);
+levels[2].addEventListener('click', objLevels.lev3);
+levels[3].addEventListener('click', objLevels.lev4);
+levels[4].addEventListener('click', objLevels.lev5);
+levels[5].addEventListener('click', objLevels.lev6);
+levels[6].addEventListener('click', objLevels.lev7);
+levels[7].addEventListener('click', objLevels.lev8);
 
+var timerE, numE;
 var arr1 = [];
 var arr2 = [];
 var arrResult = [];
@@ -30,7 +39,7 @@ var userResult = [];
 function runTimer(){
 	timerOut.style.display = 'block';
 	timer.innerHTML = timerE;
-	
+	console.log(arr1)
 	if (timerE < timerExer.value/2) timer.style.color = '#7fc641';
 	if (timerE < timerExer.value/3) timer.style.color = '#f29807';
 	if (timerE < 60) timer.style.color = 'red';
@@ -87,7 +96,8 @@ function checkResult(){
   }
 }
 
-function pushLevel2(){
+
+function pushLevel8(){
 	getInputNumber();
 	if (numExerInput.value == '' || timerExer.value == ''){
 		outMessage.style.display = 'block';
@@ -115,7 +125,163 @@ function pushLevel2(){
 
 		var br = document.createElement('br');
 
-		span.innerHTML = arr1[i] + ' + ' + arr2[i] + ' = ';
+		span.innerHTML = 'x - ' + arr1[i] +  ' = ' + arr2[i] + '; x = ';
+		arrResult[i] = arr2[i] + arr1[i];
+
+		show.appendChild(span);
+		show.appendChild(input);
+		show.appendChild(iconNo);
+		show.appendChild(iconYes);
+		show.appendChild(br);
+	}
+
+	
+	var button = document.createElement('input');
+			button.type = 'submit';
+			button.value = 'Verifica!';
+			button.id = 'button';
+	show.appendChild(button);
+
+	butt = document.getElementById('button');
+	butt.addEventListener('click', checkResult);
+	}
+}
+
+
+function pushLevel7(){
+	getInputNumber();
+	if (numExerInput.value == '' || timerExer.value == ''){
+		outMessage.style.display = 'block';
+		outMessage.innerHTML = 'Introduce-ti campurile libere...';
+		outMessage.addEventListener('click', clouseMessage);
+	} 
+	else{
+	disabledHead();
+	generateNegativeFix(100);
+	runTimer();	
+	
+	for (var i = 0; i < numE; i++){
+		var span = document.createElement('span');
+		var input = document.createElement('input');
+				input.className = 'input-result';
+				input.addEventListener('mouseout', mouseOver);
+
+		var iconNo = document.createElement('i');
+				iconNo.style.visibility = 'hidden';
+				iconNo.innerHTML = '<i class="fas fa-times"></i>';
+
+		var	iconYes = document.createElement('i');
+				iconYes.style.visibility = 'hidden';
+				iconYes.innerHTML = '<i class="fas fa-thumbs-up"></i>'
+
+		var br = document.createElement('br');
+
+		span.innerHTML = 'x + ' + arr1[i] +  ' = ' + arr2[i] + '; x = ';
+		arrResult[i] = arr2[i] - arr1[i];
+
+		show.appendChild(span);
+		show.appendChild(input);
+		show.appendChild(iconNo);
+		show.appendChild(iconYes);
+		show.appendChild(br);
+	}
+
+	
+	var button = document.createElement('input');
+			button.type = 'submit';
+			button.value = 'Verifica!';
+			button.id = 'button';
+	show.appendChild(button);
+
+	butt = document.getElementById('button');
+	butt.addEventListener('click', checkResult);
+	}
+}
+
+
+function pushLevel6(){
+	getInputNumber();
+	if (numExerInput.value == '' || timerExer.value == ''){
+		outMessage.style.display = 'block';
+		outMessage.innerHTML = 'Introduce-ti campurile libere...';
+		outMessage.addEventListener('click', clouseMessage);
+	} 
+	else{
+	disabledHead();
+	generateNegativeFix(100);
+	runTimer();	
+	
+	for (var i = 0; i < numE; i++){
+		var span = document.createElement('span');
+		var input = document.createElement('input');
+				input.style.cssText = 'margin: -70px 0 40px 0';
+				input.className = 'input-result';
+				input.addEventListener('mouseout', mouseOver);
+
+		var iconNo = document.createElement('i');
+				iconNo.style.visibility = 'hidden';
+				iconNo.innerHTML = '<i class="fas fa-times"></i>';
+
+		var	iconYes = document.createElement('i');
+				iconYes.style.visibility = 'hidden';
+				iconYes.innerHTML = '<i class="fas fa-thumbs-up"></i>'
+
+		var br = document.createElement('br');
+
+		span.innerHTML = zeroFixed(arr2[i]) + ' - <br>' + zeroFixed(arr1[i]) + '<br>&macr;&macr;<br>';
+		arrResult[i] = arr2[i] - arr1[i];
+
+		show.appendChild(span);
+		show.appendChild(input);
+		show.appendChild(iconNo);
+		show.appendChild(iconYes);
+		show.appendChild(br);
+	}
+
+	
+	var button = document.createElement('input');
+			button.type = 'submit';
+			button.value = 'Verifica!';
+			button.id = 'button';
+	show.appendChild(button);
+	show.style.cssText = 'line-height: 1.2;';
+
+	butt = document.getElementById('button');
+	butt.addEventListener('click', checkResult);
+	}
+}
+
+
+function pushLevel5(){
+	getInputNumber();
+	if (numExerInput.value == '' || timerExer.value == ''){
+		outMessage.style.display = 'block';
+		outMessage.innerHTML = 'Introduce-ti campurile libere...';
+		outMessage.addEventListener('click', clouseMessage);
+	} 
+	else{
+	disabledHead();
+	generateNum(100);
+	runTimer();	
+	
+	for (var i = 0; i < numE; i++){
+		var span = document.createElement('span');
+		var input = document.createElement('input');
+				input.style.cssText = 'margin: -70px 0 40px 0';
+				input.className = 'input-result';
+				input.addEventListener('mouseout', mouseOver);
+
+		var iconNo = document.createElement('i');
+				iconNo.style.visibility = 'hidden';
+				iconNo.innerHTML = '<i class="fas fa-times"></i>';
+
+		var	iconYes = document.createElement('i');
+				iconYes.style.visibility = 'hidden';
+				iconYes.innerHTML = '<i class="fas fa-thumbs-up"></i>'
+
+		var br = document.createElement('br');
+
+		span.innerHTML = zeroFixed(arr1[i]) + ' + <br>' + zeroFixed(arr2[i]) + '<br>&macr;&macr;<br>';
 		arrResult[i] = arr1[i] + arr2[i];
 
 		show.appendChild(span);
@@ -125,6 +291,160 @@ function pushLevel2(){
 		show.appendChild(br);
 	}
 
+	var button = document.createElement('input');
+			button.type = 'submit';
+			button.value = 'Verifica!';
+			button.id = 'button';
+	show.appendChild(button);
+	show.style.cssText = 'line-height: 1.2;';
+
+	butt = document.getElementById('button');
+	butt.addEventListener('click', checkResult);
+	}
+}
+
+
+function pushLevel4(){
+	getInputNumber();
+	if (numExerInput.value == '' || timerExer.value == ''){
+		outMessage.style.display = 'block';
+		outMessage.innerHTML = 'Introduce-ti campurile libere...';
+		outMessage.addEventListener('click', clouseMessage);
+	} 
+	else{
+	disabledHead();
+	generateNum(10);
+	runTimer();	
+	
+	for (var i = 0; i < numE; i++){
+		var span = document.createElement('span');
+		var input = document.createElement('input');
+				input.className = 'input-result';
+				input.addEventListener('mouseout', mouseOver);
+
+		var iconNo = document.createElement('i');
+				iconNo.style.visibility = 'hidden';
+				iconNo.innerHTML = '<i class="fas fa-times"></i>';
+
+		var	iconYes = document.createElement('i');
+				iconYes.style.visibility = 'hidden';
+				iconYes.innerHTML = '<i class="fas fa-thumbs-up"></i>'
+
+		var br = document.createElement('br');
+
+		span.innerHTML = 'x - ' + arr1[i] +  ' = ' + arr2[i] + '; x = ';
+		arrResult[i] = arr2[i] + arr1[i];
+
+		show.appendChild(span);
+		show.appendChild(input);
+		show.appendChild(iconNo);
+		show.appendChild(iconYes);
+		show.appendChild(br);
+	}
+
+	
+	var button = document.createElement('input');
+			button.type = 'submit';
+			button.value = 'Verifica!';
+			button.id = 'button';
+	show.appendChild(button);
+
+	butt = document.getElementById('button');
+	butt.addEventListener('click', checkResult);
+	}
+}
+
+
+function pushLevel3(){
+	getInputNumber();
+	if (numExerInput.value == '' || timerExer.value == ''){
+		outMessage.style.display = 'block';
+		outMessage.innerHTML = 'Introduce-ti campurile libere...';
+		outMessage.addEventListener('click', clouseMessage);
+	} 
+	else{
+	disabledHead();
+	generateNegativeFix(10);
+	runTimer();	
+	
+	for (var i = 0; i < numE; i++){
+		var span = document.createElement('span');
+		var input = document.createElement('input');
+				input.className = 'input-result';
+				input.addEventListener('mouseout', mouseOver);
+
+		var iconNo = document.createElement('i');
+				iconNo.style.visibility = 'hidden';
+				iconNo.innerHTML = '<i class="fas fa-times"></i>';
+
+		var	iconYes = document.createElement('i');
+				iconYes.style.visibility = 'hidden';
+				iconYes.innerHTML = '<i class="fas fa-thumbs-up"></i>'
+
+		var br = document.createElement('br');
+
+		span.innerHTML = arr2[i] + ' - ' + arr1[i] + ' = ';
+		arrResult[i] = arr2[i] - arr1[i];
+
+		show.appendChild(span);
+		show.appendChild(input);
+		show.appendChild(iconNo);
+		show.appendChild(iconYes);
+		show.appendChild(br);
+	}
+
+	
+	var button = document.createElement('input');
+			button.type = 'submit';
+			button.value = 'Verifica!';
+			button.id = 'button';
+	show.appendChild(button);
+
+	butt = document.getElementById('button');
+	butt.addEventListener('click', checkResult);
+	}
+}
+
+
+function pushLevel2(){
+	getInputNumber();
+	if (numExerInput.value == '' || timerExer.value == ''){
+		outMessage.style.display = 'block';
+		outMessage.innerHTML = 'Introduce-ti campurile libere...';
+		outMessage.addEventListener('click', clouseMessage);
+	} 
+	else{
+	disabledHead();
+	generateNegativeFix(10);
+	runTimer();	
+	
+	for (var i = 0; i < numE; i++){
+		var span = document.createElement('span');
+		var input = document.createElement('input');
+				input.className = 'input-result';
+				input.addEventListener('mouseout', mouseOver);
+
+		var iconNo = document.createElement('i');
+				iconNo.style.visibility = 'hidden';
+				iconNo.innerHTML = '<i class="fas fa-times"></i>';
+
+		var	iconYes = document.createElement('i');
+				iconYes.style.visibility = 'hidden';
+				iconYes.innerHTML = '<i class="fas fa-thumbs-up"></i>'
+
+		var br = document.createElement('br');
+
+		span.innerHTML = 'x + ' + arr1[i] +  ' = ' + arr2[i] + '; x = ';
+		arrResult[i] = arr2[i] - arr1[i];
+
+		show.appendChild(span);
+		show.appendChild(input);
+		show.appendChild(iconNo);
+		show.appendChild(iconYes);
+		show.appendChild(br);
+	}
+
+	
 	var button = document.createElement('input');
 			button.type = 'submit';
 			button.value = 'Verifica!';
@@ -194,6 +514,20 @@ function generateNum(num){
 	}
 }
 
+
+function generateNegativeFix(num){
+	for (var i = 0; i < numE; i++){
+		arr1[i] = Math.floor(Math.random() * Math.floor(num));
+		arr2[i] = Math.floor(Math.random() * Math.floor(num));
+		if (arr1[i] > arr2[i]){
+			var temp = arr1[i];
+			arr1[i] = arr2[i];
+			arr2[i] = temp;
+		}
+	}
+}
+
+
 function refrashDoc(){
 	window.location.reload();
 }
@@ -245,4 +579,10 @@ function checkStart(){
 		outMessage.innerHTML = 'Introduce-ti campurile libere...';
 	}
 		outMessage.addEventListener('click', refrashDoc);
+}
+
+
+function zeroFixed(num){
+	if (num < 10) num = '&nbsp;&nbsp;' + num;
+	return num;
 }
